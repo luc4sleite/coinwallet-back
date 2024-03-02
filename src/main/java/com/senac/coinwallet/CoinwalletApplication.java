3f20de5e3ca99b0f743b3836eac98745863a3129
luc4sleite <lucahleite@gmail.com>
2024-03-02 09:33:43 -0300

    Primeiro commit

---
+++ src/main/java/com/senac/coinwallet/CoinwalletApplication.java
@@ -0,0 +1,13 @@
+package com.senac.coinwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinwalletApplication.class, args);
+	}
+
+}
